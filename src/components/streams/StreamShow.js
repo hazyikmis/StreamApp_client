1fc051f515c5d5951d2ccaed4af603911e3f805e
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2019-07-06 03:44:09 +0300

    stream showing & OBS

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,52 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    //this.props.fetchStream(this.props.match.params.id);
+    this.props.fetchStream(id);
+    /*
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:/8000/live/${id}.flv`
+    });
+    //BE CAREFUL: When the page first rendered, there is no "this.props.stream", so only "Loading..."
+    //shown on the screen and there is NO video element. Thus, the code below throws an error...
+    //Because of that all this part moved to the under if statement inside the buildPlayer function.
+    player.attachMediaElement(this.videoRef.current);
+    player.load();
+*/
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -21,10 +63,12 @@ class StreamShow extends React.Component {
     );
 */
 
+    //So in order to get a reference to this video element right here below we're going to
+    //define our constructor method up at the top
     const { title, description } = this.props.stream;
-
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
